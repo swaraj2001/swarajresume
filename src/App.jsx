d1120e33d57db8523868134bb51762f55a14b8b2
swaraj2001 <chimurkarswaraj@gmail.com>
2023-12-20 11:40:33 +0530

    uploding files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Navbar from './Portfolio/Navbar'
+import About from './Portfolio/About'
+import Header from './Portfolio/Header'
+import Skills from './Portfolio/Skills'
+import Footer from './Portfolio/Footer'
+
+function App() {
+ 
+
+  return (
+    <div>
+     <Navbar/>
+     <Header/>
+     <About/>
+     <Skills/>
+     <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Portfolio/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css'
+function Header ()  {
+  return (
+    <div className='Header'>
+      
+    <header className="headersec">
+        <h1 id="name">SWARAJ CHIMURKAR</h1>
+        <h2 id="jobTitle">WEB DEVELOPER</h2>
+        <h2 id="contact" ><button id="headerbtn"><a href="tel:+917620057662"
+                    >CONTACT ME</a></button></h2>
+    </header>
+    
+
+    </div>
+  );
+}
+
+export default Header;
